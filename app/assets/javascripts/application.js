459ddfbc0e9de6e710696cd4e0c8b78d15359bb2
Austin <aokincaid@gmail.com>
2017-02-20 20:52:37 -0800

    Add essential Vue configuration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require vue
+//= require vue-router
+//= require vue-resource
 //= require_tree .
