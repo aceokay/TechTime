2d7e999ad3d8cf606190a3401816f1d71cfb78e2
Austin <aokincaid@gmail.com>
2017-02-21 18:10:51 -0800

    Add controller and basic route configuration

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,30 @@
+class StudentController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    @students = Student.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @students }
+    end
+  end
+
+  def create
+    @student = Student.new(student_params)
+    respond_to do |format|
+      format.json do
+        if @student.save
+          render :json => @student
+        else
+          render :json => { :errors => @student.errors.messages }, :status => 422
+        end
+      end
+    end
+  end
+
+  private
+
+  def employee_params
+    params.require(:student).permit(:name)
+  end
+end
