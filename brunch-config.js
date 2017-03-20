abd3905fc8b586765ecce420411bc84c74ada259
Austin <aokincaid@gmail.com>
2017-03-19 22:12:16 -0700

    Add font-awesome functionality

--- brunch-config.js
+++ brunch-config.js
@@ -14,10 +14,23 @@ module.exports = {
         'javascripts/brunch/vendor.js': /^node_modules/
       }
     },
-    stylesheets: {joinTo: 'stylesheets/brunch/app.css'}
+    stylesheets: {
+      joinTo: {
+        'stylesheets/brunch/app.css': /^app/,
+        'stylesheets/brunch/vendor.css': /^node_modules/
+      }
+    }
   },
   npm: {
     globals: {Vue: 'vue'},
-    globals: {VueResource: 'vue-resource'}
+    globals: {VueResource: 'vue-resource'},
+    styles: {
+      'font-awesome': ['css/font-awesome.css']
+    }
+  },
+  plugins: {
+    copycat: { // copies to priv/static/fonts/
+      fonts: ["node_modules/font-awesome/fonts"]
+    }
   }
 }
