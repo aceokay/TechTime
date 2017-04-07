967a4cd595b58e05bb86b6ba1f6de66f29c157ab
Austin <aokincaid@gmail.com>
2017-04-06 21:14:55 -0700

    Add rough, and I mean ROUGH, implementation of component in another component

--- app/brunch/students.js
+++ app/brunch/students.js
@@ -1,4 +1,32 @@
+var ListRow = {
+  props: {
+    student: Object
+  },
+  data: function() {
+    return {
+      editMode: false,
+      errors: {}
+    }
+  },
+  template: `
+  <div>
+    <div v-if="editMode">
+      <input type="text" v-model="student.name"><br>
+    </div>
+    <div v-else>
+      {{ student.name }}
+    </div>
+    <i class="fa fa-toggle-on" aria-hidden="true"></i>
+    <button v-if="editMode" @click="updateStudent">Save</button>
+    <button v-else @click="editMode = true">Edit</button>
+  </div>
+  `
+}
+
 var Students = {
+  components: {
+    'student-list-row': ListRow
+  },
   data: function() {
     return {
       students: [],
@@ -34,22 +62,24 @@ var Students = {
   },
   template: `
   <div>
-  <div class="col-sm-4">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <i class="fa fa-graduation-cap" aria-hidden="true"></i>
-        Students
+    <br>
+    <div class="col-sm-3">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+          Students
+        </div>
+        <div class="panel-body">
+          <input class="form-control" type="text" v-model="student.name" v-on:keyup.enter="addStudent" placeholder="name"><br>
+          <span style="color:red">{{ errors.name }}</span>
+          <ul class="list-unstyled">
+            <li is="student-list-row"
+              v-for="student in students"
+              :student="student">
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="panel-body">
-        <input class="form-control" type="text" v-model="student.name" v-on:keyup.enter="addStudent" placeholder="name"><br>
-        <span style="color:red">{{ errors.name }}</span>
-        <ul class="list-unstyled">
-          <li v-for="student in students">
-            {{ student.name }}
-          </li>
-        </ul>
-      </div>
-    </div>
     </div>
   </div>`
 }
