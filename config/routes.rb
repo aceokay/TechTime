801d0c93c99d6be5ab282ce94311ce6b66bef378
Austin <aokincaid@gmail.com>
2017-05-06 21:56:28 -0700

    Add new routes and set new default home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'students#index'
+  root to: 'groups#index'
+  resources :assignments, except: [:new, :edit]
+  resources :groups, except: [:new, :edit]
   resources :students, except: [:new, :edit]
 end
