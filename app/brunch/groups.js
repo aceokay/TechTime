e30c06edde04f63fc46f0a2fb4b1786996912df9
Austin <aokincaid@gmail.com>
2017-05-06 21:59:51 -0700

    Create basic Group list view with basic create functionality (and preparation for future implementations)

---
+++ app/brunch/groups.js
@@ -0,0 +1,104 @@
+var GroupListRow = {
+  props: {
+    group: Object,
+    parentUpdateGroup: Function,
+    parentDeleteGroup: Function
+  },
+  methods: {
+    updateGroup: function() {
+
+    },
+    deleteGroup: function() {
+
+    }
+  },
+  data: function() {
+    return {
+      editMode: false
+    }
+  },
+  template: `
+  <li>
+    <div>
+      <span v-if="editMode">
+        <input class="form-control" type="text" v-model="group.name" v-on:keyup.enter="updateGroup()" ><br>
+      </span>
+      <span v-else>
+        <i class="fa fa-times text-danger" @click="deleteGroup()" aria-hidden="true"></i>
+        <i class="fa fa-pencil" aria-hidden="true" @click="editMode = true"></i>
+        {{ group.name }}
+      </span>
+    </div>
+  </li>`
+}
+
+var Groups = {
+  components: {
+    'group-list-row': GroupListRow
+  },
+  data: function() {
+    return {
+      groups: [],
+      group: {
+        name: ''
+      },
+      errors: {},
+      AddGroup: false
+    }
+  },
+  mounted: function() {
+    var that = this;
+    groupResource.get().then(
+      function(response) {
+        that.groups = response.data;
+      }
+    )
+  },
+  methods: {
+    addGroup: function() {
+      var that = this;
+      groupResource.save({group: this.group}).then(
+        function(response) {
+          that.errors = {};
+          that.group = {};
+          that.groups.push(response.data);
+        },
+        function(response) {
+          that.errors = response.data.errors;
+        }
+      )
+    },
+    parentUpdateGroup: function() {
+
+    },
+    parentDeleteGroup: function() {
+
+    }
+  },
+  template: `
+  <div>
+    <br>
+    <div class="col-sm-3">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <p class="text-center">
+            <i class="fa fa-users" aria-hidden="true"></i>
+            Groups
+          </p>
+        </div>
+        <div class="panel-body">
+          <input class="form-control" type="text" v-model="group.name" v-on:keyup.enter="addGroup" placeholder="name"><br>
+          <span style="color:red">{{ errors.name }}</span>
+          <ul class="list-unstyled">
+            <li is="group-list-row"
+              v-for="group in groups"
+              :parentUpdateGroup="parentUpdateGroup"
+              :parentDeleteGroup="parentDeleteGroup"
+              :group="group">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>`
+}
--- app/brunch/vue_instance.js
+++ app/brunch/vue_instance.js
@@ -1,6 +1,8 @@
 new Vue({
   el: '#app',
   components: {
+    // 'assignments-list': Assignments,
+    'groups-list': Groups,
     'students-list': Students
   }
 });
