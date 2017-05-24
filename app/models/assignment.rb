1adfc95b327cd21247e06e29d5978c627b036b19
Austin <aokincaid@gmail.com>
2017-05-23 21:41:12 -0700

    Complete relationship between Assignment and Student Models

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,5 +1,6 @@
 class Assignment < ActiveRecord::Base
   belongs_to :group
+  has_many :students_assignments, class_name: StudentsAssignments
   has_many :students, through: :students_assignments
 
   validates :title, presence: true
