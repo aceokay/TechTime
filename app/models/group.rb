1a1e04a13cbff16ee30c5fe4ea5f106fa5eceeba
Austin <aokincaid@gmail.com>
2017-06-05 20:34:39 -0700

    Add attribute to Group class to have many assignments

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ActiveRecord::Base
   has_many :students_groups, class_name: StudentsGroups
   has_many :students, through: :students_groups
+  has_many :assignments
 
   validates :name, presence: true
 end
